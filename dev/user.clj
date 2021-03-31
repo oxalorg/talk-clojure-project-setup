b2c9f90c4deef6dc9a605ff940072daeff5bd3f3
Mitesh Shah <mitesh@miteshshah.com>
2021-04-01 01:59:22 +0530

    helper commands in user to start/stop/reset server

--- dev/user.clj
+++ dev/user.clj
@@ -1,3 +1,23 @@
-(ns user)
+(ns user
+  (:require [demo.core :refer [new-jetty]]
+            [clojure.tools.namespace.repl :refer [refresh]]
+            [ring.adapter.jetty :as jetty])
+  (:import org.eclipse.jetty.server.Server))
 
-(println "Are we inside user ns?")
+(def server (atom nil))
+
+(defn start-server []
+  (when (nil? @server)
+    (reset! server (new-jetty))))
+
+(defn stop-server []
+  (.stop @server)
+  (reset! server nil))
+
+(defn reset-server []
+  (stop-server)
+  (refresh :after 'user/start-server))
+
+(comment
+  (start-server)
+  (reset-server))
--- src/demo/core.clj
+++ src/demo/core.clj
@@ -1,17 +1,17 @@
 (ns demo.core
   (:require [clojure.data.json :as json]
             [hiccup.core :as h]
+            [ring.adapter.jetty :as jetty]
             [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.content-type :refer [wrap-content-type]]
-            [ring.adapter.jetty :as jetty])
-  (:import org.eclipse.jetty.server.Server))
+            [ring.middleware.content-type :refer [wrap-content-type]])
+  )
 
 (defn html-handler [request]
   {:status 200
    :body (h/html
           [:html
            [:body
-            [:h2 "Hello world!"]
+            [:h2 "Hello world again!"]
             [:script {:type "application/javascript" :src "/out/hello.js"}]]])})
 
 (defn json-handler [request]
@@ -29,8 +29,5 @@
    (wrap-resource handler "public")
    #_(wrap-content-type)))
 
-(def ^Server server
+(defn new-jetty []
   (jetty/run-jetty (var app) {:port 3000 :join? false}))
-
-(comment
-  (-> server .stop))
