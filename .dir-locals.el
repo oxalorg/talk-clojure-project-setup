3a68b3e800cd822c76f116691e68f10a02ea1153
Mitesh Shah <mitesh@miteshshah.com>
2021-04-02 16:20:32 +0530

    add in shadow cljs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((nil . ((cider-clojure-cli-global-options . "-A:dev")
-         )))
+         (eval . (progn
+                   (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+                   (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))))))
--- src/demo/hello.cljs
+++ src/demo/hello.cljs
@@ -3,3 +3,13 @@
 (println "Hello from println")
 (js/console.log "Hello from console.log")
 (js/alert "Hello from alert")
+
+
+(def hello (let [h4 (-> js/document
+                        (.createElement "h4"))]
+             (set! (.-innerHTML h4) "Helllooooooo")
+             h4))
+
+(println hello)
+
+(.appendChild js/document.body hello)
